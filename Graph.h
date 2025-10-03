aa00354c77f4b4af3b0434c71a06cc45b8bba48d
Fugur <74984602+Fugur@users.noreply.github.com>
2025-10-03 20:56:45 +0300

    V1.0
    Make a templates for graph
    small optimisation

--- Graph.h
+++ Graph.h
@@ -8,28 +8,25 @@ using namespace std;
 
 // todo
 // - tamplate for Vertex value
-// - cheker for existing edge
 // - DFS
-
+template<typename T>
 class Vertex {
 private:
-	int value;
+	T value;
 	int id;
 
 public:
-	Vertex(int _value, int _id) {
-		value = _value;
-		id = _id;
-	}
+	Vertex(T _value, int _id) : value(_value), id(_id){	}
 
 	int getId() const { return id; }
-	int getValue() const { return value; }
+	T getValue() const { return value; }
 };
 
+template<typename VertexType>
 class Graph{
 private:
 
-	vector<Vertex> vertexes;
+	vector<Vertex<VertexType>> vertexes;
 	vector<vector<int>> ad_list;
 	vector<vector<int>> ad_matrix;
 
@@ -38,19 +35,27 @@ public:
 
 	// добавление узлов и ребер
 
-	void addVertex(int _value) {
+	void addVertex(VertexType _value) {
 		int new_id = vertexes.size();
 		vertexes.push_back(Vertex(_value, new_id));
 		
-		expandList();
-		expandMatrix();
+		expandStructures(1);
+	}
 
-		cout << "Добавлена вершина ID: " << new_id << ", значение: " << _value << endl;
+	void addVertex(const vector<VertexType>& _vertex_values) {
+		for (VertexType vertex_value : _vertex_values) {
+			int new_id = vertexes.size();
+			vertexes.emplace_back(vertex_value, new_id);
+		}
+		expandStructures(_vertex_values.size());
 	}
 
 	void addEdge(int _first_id, int _second_id) {
 		if (!validId(_first_id)) throw out_of_range("Первый id вне диапазона");
 		if (!validId(_second_id)) throw out_of_range("Второй id вне диапазона");
+		if (std::ranges::find(ad_list[_first_id], _second_id) != ad_list[_first_id].end() || std::ranges::find(ad_list[_second_id], _first_id) != ad_list[_second_id].end()) {
+			throw invalid_argument("Ребро уже существует");
+		}
 
 		ad_list[_first_id].push_back(_second_id);
 		ad_list[_second_id].push_back(_first_id);
@@ -62,15 +67,15 @@ public:
 	}
 
 	// getters
-	Vertex getVertexById(int _target_id) const {
+	Vertex<VertexType> getVertexById(int _target_id) const {
 		if (validId(_target_id)) {
 			return vertexes[_target_id];
 		}
 		throw out_of_range("Узла с id: " + to_string(_target_id)+" не существует");
 	}
 	
-	vector<Vertex> getVertexByValue(int _target_value) const {
-		vector<Vertex> found;
+	vector<Vertex<VertexType>> getVertexByValue(const VertexType& _target_value) const {
+		vector<Vertex<VertexType>> found;
 
 		for (int i = 0; i < vertexes.size(); i++) {
 			if (vertexes[i].getValue() == _target_value) {
@@ -112,7 +117,7 @@ public:
 		while (!q.empty()) {
 			int current = q.front();
 			q.pop();
-			traversal_order.push_back(vertexes[current].getValue());
+			traversal_order.push_back(current);
 
 			// add all unvisited neighbors
 			for (int neighbor : ad_list[current]) {
@@ -129,7 +134,7 @@ public:
 	vector<int> shortestPath(int _start_id, int _target_id) const {
 		if (!validId(_start_id))throw out_of_range("start id is out of range");
 		if (!validId(_target_id)) throw out_of_range("target id is out of range");
-		if (_start_id == _target_id) return vector<int>{vertexes[_start_id].getValue()};
+		if (_start_id == _target_id) return vector<int>{_start_id};
 
 		vector<int> parent(vertexes.size(), -1);
 		vector<bool> visited(vertexes.size(), false);
@@ -160,7 +165,7 @@ public:
 
 		vector<int> path;
 		for (int at = _target_id; at > -1; at = parent[at]) {
-			path.push_back(vertexes[at].getValue());
+			path.push_back(at);
 		}
 		reverse(path.begin(), path.end());
 
@@ -168,7 +173,7 @@ public:
 	}
 //private methods
 private:
-	void expandMatrix() {
+	/*void expandMatrix() {
 		int new_size = vertexes.size();
 
 		for (auto& row : ad_matrix) {
@@ -178,7 +183,22 @@ private:
 	}	
 	void expandList() {
 		ad_list.push_back(vector<int>());
-	}
+	}*/
+
+	void expandStructures(size_t count) {
+		size_t new_size = vertexes.size();
+		//расширение матрицы
+		for (auto& row : ad_matrix) { // Расширение строк
+			row.resize(new_size, 0);
+		}
+
+		for (size_t i = 0; i < count; i++) { // Расширение столбцов
+			ad_matrix.emplace_back(new_size, 0);
+		}
+		//расширение листа
+		ad_list.resize(new_size);
+
+	};
 
 	bool validId(int _id) const {
 		return _id >= 0 && _id < vertexes.size();
